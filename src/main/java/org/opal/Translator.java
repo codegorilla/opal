cd269728e30fc03f6fca6a346a6556e4a260ee53
Gorilla <codegorilla@users.noreply.github.com>
2025-09-23 23:49:35 -0700

    Clean up translator class

--- src/main/java/org/opal/Translator.java
+++ src/main/java/org/opal/Translator.java
@@ -5,80 +5,57 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import java.util.LinkedList;
-import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Translator {
 
-  private final String directoryName;
-
-  public Translator (String directoryName) {
-    this.directoryName = directoryName;
-    loadPackage();
+  public Translator (String packageDirectory) {
+    var packagePath = getPackagePath(packageDirectory);
+    if (packagePath == null) {
+      System.out.println("error: specified package does not exist");
+      System.exit(1);
+    }
+    var filePaths = getFilePaths(packagePath);
+    // For each file, load and process
+    for (var filePath : filePaths) {
+      loadFile(filePath);
+    }
   }
 
-  private Set<Path> getFiles () {
-    // Get list of all files in the package directory
-    var packageUrl  = this.getClass().getClassLoader().getResource(directoryName);
-    var packageDir  = Objects.requireNonNull(packageUrl).getPath();
-    var packagePath = Paths.get(packageDir);
-
+  private Set<Path> getFilePaths (Path packagePath) {
+    // Given a package path
+    final var OPAL_EXTENSION = ".opc";
     try (var filePaths = Files.list(packagePath)) {
       return filePaths
         .filter(Files::isRegularFile)
-        .filter(filePath -> filePath.getFileName().toString().endsWith(".opc"))
+        .filter(filePath -> filePath.getFileName().toString().endsWith(OPAL_EXTENSION))
         .collect(Collectors.toSet());
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
   }
 
-  // This loads resources from the classpath. We actually want to load from the current directory or perhaps $OPATH
-  // because this is a CLI application. How can you make a java program look in the current directory?
-
-  private void loadPackage () {
-
-    var files = getFiles();
-    for (var item : files)
-      System.out.println(item);
+  private Path getPackagePath (String packageDirectory) {
+    // Given a package directory, return the path
+    var packagePath = Paths.get(packageDirectory).toAbsolutePath();
+    if (!Files.exists(packagePath) || !Files.isDirectory(packagePath))
+      return null;
+    else
+      return packagePath;
   }
-//    Set<Path> filesToProcess = null;
-//    // Get a list of all files in the directory and process each individually.
-//    try (var filePaths = Files.list(packagePath)) {
-//      filesToProcess = filePaths
-//        .filter(filePath -> Files.isRegularFile(filePath))
-//        .filter(filePath -> filePath.getFileName().toString().endsWith(".opc"))
-//        .collect(Collectors.toSet());
-//    } catch (IOException e) {
-//      throw new RuntimeException(e);
-//    }
 
-//    try (var filePaths = Files.list(packagePath)) {
-//      for (var filePath : filePaths.toList()) {
-//        if (Files.isRegularFile(filePath) && filePath.getFileName().toString().endsWith(".opc"))
-//          System.out.println(filePath.getFileName());
-//      }
-
-/*
-    var readPath = Paths.get(packagePath + "/" + "test.opc");
-
-    var reader = new Reader(readPath);
+  private void loadFile (Path filePath) {
+    var reader = new Reader(filePath);
     var out = reader.process();
     System.out.println(out);
-
     var lexer = new Lexer(out);
     var tokens = lexer.process();
     System.out.println(tokens);
-
     var parser = new Parser(tokens);
     var root = parser.process();
-
     var pass1 = new Pass1(root);
     pass1.process();
   }
- */
 
 }
---
+++ src/test/java/org/opal/TranslatorTest.java
@@ -0,0 +1,16 @@
+package org.opal;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+
+class TranslatorTest {
+
+  @Test
+  void doSomething () {
+    var t = new Translator("program");
+    Assertions.assertEquals(1, 1);
+  }
+
+}
