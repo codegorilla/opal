b27f7da94ac03d55f638d4ecc909518b05c323b5
Gorilla <codegorilla@users.noreply.github.com>
2025-10-10 06:54:52 -0700

    Clean up code generation of entry point

--- src/main/java/org/opal/Generator1.java
+++ src/main/java/org/opal/Generator1.java
@@ -3,22 +3,29 @@ package org.opal;
 import org.opal.ast.AstNode;
 import org.opal.ast.TranslationUnit;
 import org.opal.ast.declaration.*;
-import org.opal.ast.expression.*;
-import org.opal.ast.type.*;
 import org.stringtemplate.v4.ST;
 import org.stringtemplate.v4.STGroupDir;
 
 import java.net.URL;
 import java.util.HashMap;
-import java.util.LinkedList;
 
-// The purpose of this pass is to create a module interface unit.
+// The purpose of this pass is to create the entry point. The entry point must
+// be created with a main function that matches the main function defined in
+// the main module.
+
+// In C++, the entry point main function must be defined in the global module
+// fragment, outside of any module. However, we can define other functions
+// named main as long as they are placed inside namespaces, which distinguishes
+// them from the entry point main function. We can leverage this fact to
+// implement opal's design of having its "entry point" main function defined
+// inside of its main module.
 
 public class Generator1 extends BaseVisitor {
 
   private final URL templateDirectoryUrl;
   private final STGroupDir group;
 
+  // Tracks whether or not methods are named 'main'
   private HashMap<AstNode, Boolean> isMain = new HashMap<>();
 
   // Assume nullary (zero-argument) entry point unless proven otherwise
@@ -40,7 +47,7 @@ public class Generator1 extends BaseVisitor {
 
   public void visit (TranslationUnit node) {
     visit(node.declarations());
-    ST st = group.getInstanceOf((nullaryEntryPoint ? "translationUnit0" : "translationUnit1"));
+    ST st = group.getInstanceOf("translationUnit" + (nullaryEntryPoint ? 0 : 1));
     System.out.println("---");
     System.out.println(st.render());
   }
