7793e974ba705b619a4a9b84a7c43bfe06b718b2
Gorilla <codegorilla@users.noreply.github.com>
2025-10-19 18:05:27 -0700

    Add to do comment about aggregating declarations and definitions from multiple translation units

--- src/main/java/org/opal/Generator3.java
+++ src/main/java/org/opal/Generator3.java
@@ -2,15 +2,10 @@ package org.opal;
 
 import org.opal.ast.AstNode;
 import org.opal.ast.TranslationUnit;
-import org.opal.ast.declaration.*;
-import org.opal.ast.expression.*;
-import org.opal.ast.statement.*;
-import org.opal.ast.type.*;
 import org.stringtemplate.v4.ST;
 import org.stringtemplate.v4.STGroupDir;
 
 import java.net.URL;
-import java.util.LinkedList;
 
 // The purpose of this pass is to aggregate declarations and definitions for
 // the module implementation unit.
@@ -19,18 +14,6 @@ public class Generator3 extends ResultBaseVisitor <ST> {
 
   private final URL templateDirectoryUrl;
   private final STGroupDir group;
-
-  // Stack for facilitating out-of-order operations. For example, we need to swap the base type for the variable name in
-  // order to form a declarator. We also need to invert the order in which arrays and pointers are processed.
-  private final LinkedList<ST> stack = new LinkedList<>();
-
-  // Stack for keeping track of current node path
-  private final LinkedList<AstNode> ancestorStack = new LinkedList<>();
-
-  private int pass = 1;
-
-  // Tracks modifier passes
-  private int modifiersPass = 0;
 
   public Generator3 (AstNode input) {
     super(input);
@@ -47,11 +30,13 @@ public class Generator3 extends ResultBaseVisitor <ST> {
   }
 
   public ST visit (AstNode node) {
-    ancestorStack.push(node);
-    var st = node.accept(this);
-    ancestorStack.pop();
-    return st;
+    return node.accept(this);
   }
+
+  // To do: We need to accumulate declarations from all translation units into
+  // one list and their corresponding definitions into another list. Then write
+  // to file all declarations, followed by all definitions. The following code
+  // doesn't yet handle multiple files.
 
   public ST visit (TranslationUnit node) {
     var st = group.getInstanceOf("implementation/translationUnit");
@@ -64,5 +49,4 @@ public class Generator3 extends ResultBaseVisitor <ST> {
     st.add("definitions", generator3b.process());
     return st;
   }
-
 }
