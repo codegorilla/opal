5924f341616ff1398b84f26fa769fc14034ffa8a
Gorilla <codegorilla@users.noreply.github.com>
2025-11-07 19:53:54 -0800

    Finalize refinement of use-import-synthesis

--- src/main/java/org/opal/Pass20.java
+++ src/main/java/org/opal/Pass20.java
@@ -12,70 +12,38 @@ import java.util.List;
 // The purpose of this pass is to create import declarations from use
 // declarations. This avoids an extra sub-pass during code generation.
 
-public class Pass20 extends BaseVisitor {
+public class Pass20 extends BaseResultVisitor<AstNode> {
 
   private final List<String> sourceLines;
 
   // Stack for keeping track of current node path
   private final LinkedList<AstNode> nodePath = new LinkedList<>();
 
-  // Stack for passing nodes up and down during traversal
-  private final LinkedList<AstNode> nodeStack = new LinkedList<>();
-
-  // Stack for passing name information up and down during traversal
-  private final LinkedList<String> nameStack = new LinkedList<>();
-
-  // Map that relates alias name to import alias state machine
-  // This needs to either synthesize AST nodes or be passed to code generator
-  // to be used for generating namespace aliases. Or, we can go back through
-  // and attach to each import declaration node the appropriate alias name.
-  // This last option actually sounds like the best plan.
-  private final HashMap<String, ImportAliasContext> aliasMachineTable = new HashMap<>();
-
   public Pass20 (AstNode input, List<String> sourceLines) {
     super(input);
     this.sourceLines = sourceLines;
   }
 
-  public void process () {
+  public AstNode process () {
     visit(root);
+    return null;
   }
 
-  public void visit (AstNode node) {
+  public AstNode visit (AstNode node) {
     nodePath.push(node);
-    node.accept(this);
+    var n = node.accept(this);
     nodePath.pop();
+    return n;
   }
 
-  public void visit (TranslationUnit node) {
+  public AstNode visit (TranslationUnit node) {
     visit(node.declarations());
+    return null;
   }
 
-  public void visit (Declarations node) {
+  public AstNode visit (Declarations node) {
     visit(node.useDeclarations());
-    visit(node.importDeclarations());
-  }
-
-  public void visit (UseDeclarations node) {
-    for (var useDeclaration : node.getChildren())
-      visit(useDeclaration);
-  }
-
-  public void visit (UseDeclaration node) {
-    visit(node.useQualifiedName());
-  }
-
-  public void visit (UseQualifiedName node) {
-    var n = new ImportQualifiedName(null);
-    for (var useName : node.getChildren()) {
-      visit(useName);
-      n.addChild(nodeStack.pop());
-    }
-  }
-
-  public void visit (UseName node) {
-    var n = new ImportName(node.getToken());
-    nodeStack.push(n);
+    return null;
   }
 
   // To do: Although it seems that C++ can handle multiple import declarations
@@ -84,12 +52,29 @@ public class Pass20 extends BaseVisitor {
   // compare them to any existing qualified names. Such qualified names can be
   // kept in a set data structure to ensure uniqueness.
 
-  public void visit (ImportDeclarations node) {
-    while (!nodeStack.isEmpty()) {
-      var n = new ImportDeclaration(null);
-      n.addChild(nodeStack.pollLast());
-      node.addChild(n);
-    }
+  public AstNode visit (UseDeclarations node) {
+    var n = ((Declarations)nodePath.get(1)).importDeclarations();
+    for (var useDeclaration : node.getChildren())
+      n.addChild(visit(useDeclaration));
+    return null;
+  }
+
+  public AstNode visit (UseDeclaration node) {
+    var n = new ImportDeclaration(null);
+    n.addChild(visit(node.useQualifiedName()));
+    return n;
+  }
+
+  public AstNode visit (UseQualifiedName node) {
+    var n = new ImportQualifiedName(null);
+    for (var useName : node.getChildren())
+      n.addChild(visit(useName));
+    return n;
+  }
+
+  public AstNode visit (UseName node) {
+    var n = new ImportName(node.getToken());
+    return n;
   }
 
 }
